dc248ec01cc9da33ae822c5f55809aa848d96949
mvldn <mvldn@users.noreply.github.com>
2021-02-22 22:43:22 +0100

    User Story 6 – Export to-dos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,33 +14,52 @@ export class AppComponent {
   popupOpen: boolean = false;
   select: number = 0;
 
-  constructor(private toDoService: ToDoService){
+  constructor(private toDoService: ToDoService) {
   }
-  ngOnInit():void{
+  ngOnInit(): void {
     this.listOfTasks = this.toDoService.getTasks();
   }
 
-  submitNew(input: string){ 
-    this.toDoService.set({entry: input, status: false, taskNumber: 1});    
+  submitNew(input: string) {
+    this.toDoService.set({ entry: input, status: false, taskNumber: 1 });
     this.ngOnInit();
   }
 
-  onDelete(index: number){    
+  onDelete(index: number) {
     this.toDoService.remove(index);
     this.popupOpen = !this.popupOpen;
     this.select = index;
     this.ngOnInit();
   }
 
-  onClick(ind: number){
-    this.select = ind;    
+  onClick(ind: number) {
+    this.select = ind;
     this.popupOpen = !this.popupOpen;
     this.ngOnInit();
   }
 
-  onUpdateValue(keyNum: number){
+  onUpdateValue(keyNum: number) {
     this.toDoService.update(JSON.stringify(keyNum));
     this.ngOnInit();
+  }
+
+  onExport() {
+    const data = JSON.stringify(this.listOfTasks);
+    let saveData = (function () {
+      let a = document.createElement("a");
+      document.body.appendChild(a);
+      return function (data, fileName) {
+        var json = JSON.stringify(data),
+          blob = new Blob([json], { type: "octet/stream" }),
+          url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+      };
+    }());
+
+    saveData(data, "todo_export.txt");
   }
 
 }
